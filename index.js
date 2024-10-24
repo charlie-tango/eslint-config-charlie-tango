0351800cb7ee845ffa2a37e100a6100e2bd299ac
Daniel Schmidt <dsc@charlietango.dk>
2024-10-24 14:36:46 +0200

    feat: convert to eslint flat config

--- index.js
+++ index.js
@@ -1,49 +1,6 @@
-/**
- * @type {import('eslint').Config}
- */
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  parser: "@typescript-eslint/parser",
-  extends: [
-    require.resolve("./rules/base"),
-    require.resolve("./rules/react"),
-    require.resolve("./rules/typescript"),
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended",
-    "plugin:tailwindcss/recommended",
-  ],
-  plugins: ["import", "jsx-a11y", "react", "@typescript-eslint", "tailwindcss"],
-  overrides: [
-    {
-      files: ["src/**/__tests__/**/*", "src/**/*.{spec,test}.*"],
-      extends: ["plugin:testing-library/react"],
-      rules: {
-        // Disable https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-node-access.md
-        // It is a good rule, but currently there's not really a viable alternative if you need to look at the DOM (with something like `.closest())
-        "testing-library/no-node-access": "off",
-      },
-    },
-  ],
-  rules: {
-    // https://github.com/francoismassart/eslint-plugin-tailwindcss
-    'tailwindcss/classnames-order': 'off', // We use prettier to sort classes
-  },
-};
+import tseslint from "typescript-eslint";
+import baseRules from "./rules/typescript.js";
+import tsRules from "./rules/typescript.js";
+import reactRules from "./rules/react.js";
+
+export default tseslint.config(...baseRules, ...tsRules, ...reactRules);
--- readme.md
+++ readme.md
@@ -1,23 +1,25 @@
-# eslint-config-charlie-tango
+# @charlietango/eslint-config
 
-Provide a baseline of ESlint rules for projects written with TypeScript and React.
+Provide a baseline configuration and set of ESlint rules for projects written with TypeScript and React.
 
 ## Usage
 
-Create a new `.eslintrc` file, and extend the `eslint-config-charlie-tango` inside it. 
+Create a new `eslint.config.js` file, and extend the `@charlietango/eslint-config` inside it.
 
-```json
-{
-  "extends": ["eslint-config-charlie-tango"]
-}
+```js
+import ctConfig from "@charlietango/eslint-config";
+
+export default [...ctConfig];
 ```
+
+You can modify the rules as needed, or add additional rules to the configuration.
 
 ## Rules
 
 Provides configuration for the following:
 
-* JavaScript
-* TypeScript
-* React
-* React Testing Library
-* Storybook
+- JavaScript
+- TypeScript
+- React
+
+All the rules are located under `@charlietango/eslint-config/rules`, and can be imported and used individually.
